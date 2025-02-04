b79b2b7f602e52ae65c0641011f7a7156663e338
Paige Saint Germain <paigejerin@gmail.com>
2025-02-03 19:59:13 -0500

    completed JS assignment 05

--- js/assignment.js
+++ js/assignment.js
@@ -1,67 +1,100 @@
-"use strict";
+'use strict';
 
 const todos = [
-    "Wash the dishes",
-    "Clean the gutters",
-    "Mow the yard",
-    "Do some coding",
-    "Rake the leaves",
+    'Wash the dishes',
+    'Clean the gutters',
+    'Mow the yard',
+    'Do some coding',
+    'Rake the leaves',
 ];
 
 // Declare your two immutable variables
-
+let shiftValue;
+let popValue;
 
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
-const answerOneEl = document.getElementById("answer-one");
-const answerTwoEl = document.getElementById("answer-two");
-const answerThreeEl = document.getElementById("answer-three");
-const answerFourEl = document.getElementById("answer-four");
-const answerFiveEl = document.getElementById("answer-five");
-const answerSixEl = document.getElementById("answer-six");
-const submissionBtn = document.getElementById("submission-btn");
+const answerOneEl = document.getElementById('answer-one');
+const answerTwoEl = document.getElementById('answer-two');
+const answerThreeEl = document.getElementById('answer-three');
+const answerFourEl = document.getElementById('answer-four');
+const answerFiveEl = document.getElementById('answer-five');
+const answerSixEl = document.getElementById('answer-six');
+const submissionBtn = document.getElementById('submission-btn');
 
 function updateAnswerOne() {
-    
+    answerOneEl.innerHTML = todos;
 }
 
 function updateAnswerTwo() {
-
+    answerTwoEl.innerHTML = todos[2];
 }
 
 function removeLastValue() {
+    popValue = [];
+    const stored = todos.pop();
+    popValue.push(stored);
 
-}
-
-function removeFirstValue() {
-
-}
-
-function addShiftAndPopValues() {
-
+    /*
+        // After some refactoring:
+        popValue = [todos.pop()];
+    */
 }
 
 function updateAnswerFour() {
-
+    answerFourEl.innerHTML = todos;
 }
 
 function reverseTodoList() {
-
+    todos.reverse();
 }
 
 function updateAnswerFive() {
-
+    answerFiveEl.innerHTML = todos;
 }
 
-function updateAnswerSix() {
+function removeFirstValue() {
+    shiftValue = [];
+    const stored = todos.shift();
+    shiftValue.push(stored);
 
+    /*
+        // After some refactoring:
+        shiftValue = [todos.shift()];
+    */
+}
+
+// Expected is wrong?
+function addShiftAndPopValues() {
+    todos.push(popValue);
+    todos.push(shiftValue);
+
+    // This matches the expected?
+    // todos.push(popValue);
+    // todos.push(shiftValue);
+}
+
+
+function updateAnswerSix() {
+    answerSixEl.innerHTML = todos;
+
+    // Learned about join. That would be nice if I needed a space after the comma.
+    // answerSixEl.innerHTML = todos.join(', ');
 }
 
 function render() {
-
+    updateAnswerOne();
+    updateAnswerTwo();
+    removeLastValue();
+    updateAnswerFour();
+    reverseTodoList();
+    updateAnswerFive();
+    removeFirstValue();
+    addShiftAndPopValues();
+    updateAnswerSix();
 }
 
-submissionBtn.addEventListener("click", function () {
+submissionBtn.addEventListener('click', function () {
     // Update this function
     render();
 });
--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+// let shoppingList = ['Apricots', 'Pineapple', 'Raisins'];
+// console.log(shoppingList);
+
+// // Create an array that holds different data types and log it the console. What happens?
+// const array = ['string', 12, true, ['nested', 'array']];
+// console.log(array);
+
+// // Log the typeof to one array to the console. What is returned?
+// console.log(typeof array);
+
+// // Remove an item from your arrays.
+// array.shift();
+// console.log(array);
+
+// // Add an item from your arrays.
+// array.push(shoppingList[0]);
+// console.log(array);
+
+// // Change the value of an element in each array.
+// array[3] = 'Beans';
+// console.log(array);
